ad8e926f3cb000853f0743e92264585ade0dff38
smdilyass <smdilyass@gmail.com>
2024-12-26 15:06:45 +0100

    crud d'une entité en php procédural

---
+++ CRUD PHP procédural/crud/creat.php
@@ -0,0 +1,5 @@
+<?php
+include_once("connect.php");
+
+$sql ="INSERT INTO USERS VALUES ('ilyass','samoudi','email@gmail.com','password') ";
+?>
---
+++ CRUD PHP procédural/crud/update.php
@@ -0,0 +1,3 @@
+<?php
+$sql ="UPDATE firstname FROM USERS ";
+?>
